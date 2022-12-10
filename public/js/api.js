ddf27665d969ce1390611870604ead5daa8fe9da
Alerrandro Janio <alerrandro.janio07@gmail.com>
2022-12-10 13:44:55 -0300

    catalogo finalizado

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "0a1a9bb80e9156974c761c157f50ed54";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
